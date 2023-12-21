5496d14fd3348114136782a502bafdc1f1b82f2a
rishanzihad <138320123+rishanzihad@users.noreply.github.com>
2023-12-21 13:25:05 +0600

    hooks create

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import router from './Router/router'
 import { RouterProvider } from 'react-router-dom'
+import AuthProvider from './AuthProvider/AuthProvider'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+  <AuthProvider>
   <RouterProvider router={router} />
+  </AuthProvider>
   </React.StrictMode>,
 )
