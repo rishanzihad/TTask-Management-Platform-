059dfc8ccb5498f9e2156555c424d423f4bdff6e
rishanzihad <138320123+rishanzihad@users.noreply.github.com>
2023-12-21 13:34:16 +0600

    authprovider done

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -1,11 +1,66 @@
-import React from 'react';
+import { createContext, useEffect, useState } from "react";
+import {   GoogleAuthProvider, createUserWithEmailAndPassword,  getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile,  } from "firebase/auth";
+import useAxiosPublic from "../Hooks/useAxiosPublic";
+import app from "../../firebase.config";
 
-const AuthProvider = () => {
-    return (
-        <div>
+export const AuthContext =createContext(null)
+const auth = getAuth(app)
+const googleProvider =new GoogleAuthProvider
+const AuthProvider = ({children}) => {
+    const[user,setUser] =useState()
+    const axiosPublic =useAxiosPublic()
+    const [loading,setLoading] =useState(true)
+
+    const googleLogin=()=>{
+        setLoading(true)
+       return signInWithPopup(auth,googleProvider)
+    }
+    const register =(email,password)=>{
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth,email,password)
+    }
+
+    const  login =(email,password)=>{
+        return signInWithEmailAndPassword(auth,email,password)
+    }
+
+    const logOut =()=>{
+        setLoading(true)
+        signOut(auth)
+    }
+    useEffect(()=>{
+        const unsubscribe =onAuthStateChanged(auth,currentUser=>{
+            setUser(currentUser)
+            if(currentUser){
+                const userInfo={
+                   email:currentUser.email 
+                }
+                axiosPublic.post('/jwt',userInfo)
+                .then(res=>{
+                    if(res.data.token){
+                        localStorage.setItem('access-token',res.data.token)
+                        setLoading(false)
+                    }
+                })
+            }
+            else{
+                localStorage.removeItem('access-token')
+                setLoading(false)
+            }
             
-        </div>
-    );
+        })
+        return ()=>{
+            return unsubscribe
+        }
+    },[])
+    const authInfo={
+        googleLogin,register,login,user,logOut,loading,
+    }
+    return (
+        <AuthContext.Provider value={authInfo}>
+         {children}
+        </AuthContext.Provider>
+     );
 };
 
 export default AuthProvider;
